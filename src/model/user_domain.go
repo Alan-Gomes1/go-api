5841ba1d5ba681d6b4767e250aad7a6111073c2c
Alan Gomes <alan.gomes.ag28@gmail.com>
2025-07-28 17:43:30 -0300

    refact: user domain

--- src/model/user_domain.go
+++ src/model/user_domain.go
@@ -1,72 +1,33 @@
 package model
 
-import (
-	"crypto/md5"
-	"encoding/hex"
-	"encoding/json"
-
-	"github.com/Alan-Gomes1/go-api/src/configuration/logger"
-)
-
-type UserDomainInterface interface {
-	GetEmail() string
-	GetPassword() string
-	GetName() string
-	GetAge() int8
-	GetJSONValue() (string, error)
-	SetID(id string)
-	EncryptPassword()
-}
-
 type userDomain struct {
-	ID       string `json:"id"`
-	Email    string `json:"email"`
-	Password string `json:"password"`
-	Name     string `json:"name"`
-	Age      int8   `json:"age"`
+	id       string
+	email    string
+	password string
+	name     string
+	age      int8
 }
 
-func NewUserDomain(email, password, name string, age int8) UserDomainInterface {
-	return &userDomain{
-		Email: email,
-		Password: password,
-		Name: name,
-		Age:  age,
-	}
+func (u *userDomain) GetID() string {
+	return u.id
 }
 
 func (u *userDomain) GetEmail() string {
-	return u.Email
+	return u.email
 }
 
 func (u *userDomain) GetPassword() string {
-	return u.Password
+	return u.password
 }
 
 func (u *userDomain) GetName() string {
-	return u.Name
+	return u.name
 }
 
 func (u *userDomain) GetAge() int8 {
-	return u.Age
-}
-
-func (u *userDomain) GetJSONValue() (string, error) {
-	b, err := json.Marshal(u)
-	if err != nil {
-		logger.Error("Error trying to marshal user", err)
-		return "", err
-	}
-	return string(b), nil
+	return u.age
 }
 
 func (u *userDomain) SetID(id string) {
-	u.ID = id
-}
-
-func (u *userDomain) EncryptPassword() {
-	hash := md5.New()
-	defer hash.Reset()
-	hash.Write([]byte(u.Password))
-	u.Password = hex.EncodeToString(hash.Sum(nil))
+	u.id = id
 }
