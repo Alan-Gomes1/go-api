088defbc60ed523ebfb523581be1d0975a8880c0
Alan Gomes <alan.gomes.ag28@gmail.com>
2025-07-31 09:23:08 -0300

    refact: CreateUserServices

--- src/model/service/create_user.go
+++ src/model/service/create_user.go
@@ -1,13 +1,23 @@
 package service
 
 import (
+	"github.com/Alan-Gomes1/go-api/src/configuration/logger"
 	"github.com/Alan-Gomes1/go-api/src/configuration/rest_err"
 	"github.com/Alan-Gomes1/go-api/src/model"
+	"go.uber.org/zap"
 )
 
-func (u *userDomainService) CreateUser(
+func (u *userDomainService) CreateUserServices(
 	userDomain model.UserDomainInterface,
-) *rest_err.Errors {
+) (model.UserDomainInterface, *rest_err.Errors) {
 	userDomain.EncryptPassword()
-	return nil
+	userDomainRepository, err := u.userRepository.CreateUser(userDomain)
+	if err != nil {
+		logger.Error(
+			"Error trying to call repository", err,
+			zap.String("caller", "CreateUserServices"),
+		)
+		return nil, err
+	}
+	return userDomainRepository, nil
 }
