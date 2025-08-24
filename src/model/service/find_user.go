06703819e824d98f6b24dd26980eb19742184c58
Alan Gomes <alan.gomes.ag28@gmail.com>
2025-08-24 18:48:14 -0300

    refact: fixes user retrieval by adding methods for ID and email

--- src/model/service/find_user.go
+++ src/model/service/find_user.go
@@ -5,8 +5,14 @@ import (
 	"github.com/Alan-Gomes1/go-api/src/model"
 )
 
-func (u *userDomainService) FindUser(string) (
-	*model.UserDomainInterface, *rest_err.Errors,
-) {
-	return nil, nil
+func (u *userDomainService) FindUserByIDServices(
+	id string,
+) (model.UserDomainInterface, *rest_err.Errors) {
+	return u.userRepository.FindUserByID(id)
+}
+
+func (u *userDomainService) FindUserByEmailServices(
+	email string,
+) (model.UserDomainInterface, *rest_err.Errors) {
+	return u.userRepository.FindUserByEmail(email)
 }
