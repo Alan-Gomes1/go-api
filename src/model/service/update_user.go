21b208d59ca11df5b74c123c274933d71f1aae79
Alan Gomes <alan.gomes.ag28@gmail.com>
2025-08-25 16:58:30 -0300

    feat: add UpdateUserServices

--- src/model/service/update_user.go
+++ src/model/service/update_user.go
@@ -1,12 +1,20 @@
 package service
 
 import (
+	"github.com/Alan-Gomes1/go-api/src/configuration/logger"
 	"github.com/Alan-Gomes1/go-api/src/configuration/rest_err"
 	"github.com/Alan-Gomes1/go-api/src/model"
+	"go.uber.org/zap"
 )
 
-func (u *userDomainService) UpdateUser(
+func (u *userDomainService) UpdateUserServices(
 	userId string, userDomain model.UserDomainInterface,
 ) *rest_err.Errors {
+	caller := zap.String("caller", "UpdateUserServices")
+	err := u.userRepository.UpdateUser(userId, userDomain)
+	if err != nil {
+		logger.Error("Error trying to call repository", err, caller)
+		return err
+	}
 	return nil
 }
