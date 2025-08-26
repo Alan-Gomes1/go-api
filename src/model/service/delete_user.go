9280fb760bd9a2bdd1c12a6f792a74a73e230eed
Alan Gomes <alan.gomes.ag28@gmail.com>
2025-08-26 17:44:09 -0300

    feat: adding DeleteUserServices

--- src/model/service/delete_user.go
+++ src/model/service/delete_user.go
@@ -1,7 +1,17 @@
 package service
 
-import "github.com/Alan-Gomes1/go-api/src/configuration/rest_err"
+import (
+	"github.com/Alan-Gomes1/go-api/src/configuration/logger"
+	"github.com/Alan-Gomes1/go-api/src/configuration/rest_err"
+	"go.uber.org/zap"
+)
 
-func (u *userDomainService) DeleteUser(string) *rest_err.Errors {
+func (u *userDomainService) DeleteUserServices(userId string) *rest_err.Errors {
+	caller := zap.String("caller", "DeleteUserServices")
+	err := u.userRepository.DeleteUser(userId)
+	if err != nil {
+		logger.Error("Error trying to call repository", err, caller)
+		return err
+	}
 	return nil
 }
