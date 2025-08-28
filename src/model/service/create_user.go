27c7cffb1eb322440f06a028bea83e6bd7abafc7
Alan Gomes <alan.gomes.ag28@gmail.com>
2025-08-28 18:08:30 -0300

    fix: update CreateUser

--- src/model/service/create_user.go
+++ src/model/service/create_user.go
@@ -10,13 +10,16 @@ import (
 func (u *userDomainService) CreateUserServices(
 	userDomain model.UserDomainInterface,
 ) (model.UserDomainInterface, *rest_err.Errors) {
+	caller := zap.String("caller", "CreateUserServices")
+	user, _ := u.FindUserByEmailServices(userDomain.GetEmail())
+	if user != nil {
+		logger.Error("Email already registered", nil, caller)
+		return nil, rest_err.NewBadRequestError("Email already registered")
+	}
 	userDomain.EncryptPassword()
 	userDomainRepository, err := u.userRepository.CreateUser(userDomain)
 	if err != nil {
-		logger.Error(
-			"Error trying to call repository", err,
-			zap.String("caller", "CreateUserServices"),
-		)
+		logger.Error("Error trying to call repository", err, caller)
 		return nil, err
 	}
 	return userDomainRepository, nil
