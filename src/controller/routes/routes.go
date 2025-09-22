319e132daf2638d6c1f6f1725281d220ff456551
Alan Gomes <alan.gomes.ag28@gmail.com>
2025-09-21 22:28:28 -0300

    feat: adds token verification middleware to user routes

--- src/controller/routes/routes.go
+++ src/controller/routes/routes.go
@@ -2,12 +2,13 @@ package routes
 
 import (
 	"github.com/Alan-Gomes1/go-api/src/controller"
+	"github.com/Alan-Gomes1/go-api/src/model"
 	"github.com/gin-gonic/gin"
 )
 
 func InitRoutes(r *gin.RouterGroup, userController controller.UserControllerInterface) {
-	r.GET("/user/:userId", userController.FindUserById)
-	r.GET("/userByEmail/:userEmail", userController.FindUserByEmail)
+	r.GET("/user/:userId", model.VerifyTokenMIddleware, userController.FindUserById)
+	r.GET("/userByEmail/:userEmail", model.VerifyTokenMIddleware, userController.FindUserByEmail)
 	r.POST("/createUser", userController.CreateUser)
 	r.PUT("/updateUser/:userId", userController.UpdateUser)
 	r.DELETE("/deleteUser/:userId", userController.DeleteUser)
